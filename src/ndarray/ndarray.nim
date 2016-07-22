527fb648c27dc5b1bec3b23f35d9b7697daaf713
Erin Sheldon <erin.sheldon@gmail.com>
2016-07-22 08:31:48 -0400

    working on general array access

--- src/ndarray/ndarray.nim
+++ src/ndarray/ndarray.nim
@@ -1,11 +1,15 @@
 # TODO:  many things, but first
 #    - add ordering, e.g. row major vs col major
+
+import sequtils
 import strutils
 
 type
     NDArray*[T] = object
         dims: seq[int]
         ndim:int
+
+        strides: seq[int]
 
         data: seq[T]
         size: int
@@ -16,6 +20,7 @@ proc newArray*[T](self: var NDArray[T], dims: varargs[int]) =
     let ndim = len(dims)
 
     newSeq(self.dims, ndim)
+    newSeq(self.strides, ndim)
     self.ndim=ndim
 
     var size=1
@@ -23,8 +28,16 @@ proc newArray*[T](self: var NDArray[T], dims: varargs[int]) =
         self.dims[i] = dims[i]
         size = size * dims[i]
 
+    var total_stride=1
+    for i in countdown(ndim-1,0):
+
+        if i < ndim-1:
+            total_stride *= dims[i+1]
+        self.strides[i] = total_stride
+
     newSeq(self.data, size)
     self.size=size
+
 
 proc newArray*[T](dims: varargs[int]): NDArray[T] =
     newArray(result, dims)
@@ -32,7 +45,9 @@ proc newArray*[T](dims: varargs[int]): NDArray[T] =
 
 proc ones*[T](dims: varargs[int]): NDArray[T] =
     newArray[T](result,dims)
-    result.data=T(1)
+
+    for i in 0..<result.size:
+        result.data[i] = T(1)
 
 proc range*[T](dims: varargs[int]): NDArray[T] =
     newArray[T](result, dims)
@@ -56,7 +71,10 @@ template `[]`*[T](self: NDArray[T], i, j: int): auto =
     if ndim != 2:
         raise newException(IndexError,
                            "tried to index $1 dimensional array with 2 indices" % $ndim)
-    self.data[i*self.dims[1] + j]
+    self.data[
+        i*self.strides[0] +
+        j
+    ]
 
 template `[]`*[T](self: NDArray[T], i, j, k: int): auto =
     let ndim=self.ndim
@@ -65,8 +83,8 @@ template `[]`*[T](self: NDArray[T], i, j, k: int): auto =
         raise newException(IndexError,
                            "tried to index $1 dimensional array with 2 indices" % $ndim)
     self.data[
-        i*self.dims[2]*self.dims[1] +
-        j*self.dims[2]              +
+        i*self.strides[0] +
+        j*self.strides[1] +
         k
     ]
 
@@ -76,12 +94,33 @@ template `[]`*[T](self: NDArray[T], i, j, k, l: int): auto =
     if ndim != 4:
         raise newException(IndexError,
                            "tried to index $1 dimensional array with 2 indices" % $ndim)
+
     self.data[
-        i*self.dims[3]*self.dims[2]self.dims[1] +
-        j*self.dims[3]*self.dims[2]             +
-        k*self.dims[3]                          +
+        i*self.strides[0] +
+        j*self.strides[1] +
+        k*self.strides[2] +
         l
     ]
+
+template `[]`*[T](self: NDArray[T], indices: varargs[int]): auto =
+    ## general element get
+    ##
+    ## might be slower than the specific ones above but
+    ## I haven't actually timed it
+
+    let ndim=self.ndim
+    let nind=len(indices)
+
+    if ndim != nind:
+        let mess="tried to index $1 dimensional array with $2 indices" % [$ndim, $nind]
+        raise newException(IndexError, mess)
+
+    var index=0
+    for i in 0..ndim-1:
+        index += self.strides[i]*indices[i]
+
+    self.data[index]
+
 
 proc `$`*[T](self: NDArray[T]): string =
 
@@ -112,24 +151,16 @@ proc `$`*[T](self: NDArray[T]): string =
     #result = $self.data
 
 
-# general version
-#template `[]`*(self: NDArray[T], indices: varargs[int]): auto =
-#
-#    var index=0
-#    for i in indices:
-#        index = index + self.strides[i]*indices[i]
-#
-#    self.data[i * m.N + j]
-
 when isMainModule:
 
     var arr = newArray[float](3,4)
 
-    for i in 0..arr.dims[0]-1:
-        for j in 0..arr.dims[1]-1:
-            echo(arr[i,j])
     echo("arr:")
     echo(arr)
+
+    var oa=ones[float](3,3)
+    echo("ones:")
+    echo(oa)
 
     var rng = range[float](3,4)
     echo("rng:")
@@ -137,4 +168,12 @@ when isMainModule:
     echo("rng[1,1]: ",rng[1,1])
     echo("rng[1,2]: ",rng[1,2])
 
-    echo("rng[1]: ",rng[1])
+    var arr4 = range[float](3,4,5,6)
+    echo("arr4 strides:",arr4.strides)
+    echo("arr4[2,1,3,4]: ",arr4[2,1,3,4])
+
+    var arr5 = range[float](3,4,5,6,7)
+    echo("arr5 strides:",arr5.strides)
+    echo("arr5[2,1,3,4,2]: ",arr5[2,1,3,4,2])
+
+
