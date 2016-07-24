48a25663efb43ca814b0cfde211a7daf603039bf
Erin Sheldon <erin.sheldon@gmail.com>
2016-07-24 11:24:46 -0400

    use new init

--- src/gmix/gmix.nim
+++ src/gmix/gmix.nim
@@ -6,6 +6,7 @@ import hashes
 import gauss2d
 import shape
 import strutils
+import random
 
 type
     GMix* = seq[Gauss2D]
@@ -84,63 +85,65 @@ proc newGMix*(num: int) : GMix =
 #        of GMixModel.dev:
 #            echo "dev"
 
-const pvals_exp = @[
-    0.00061601229677880041,
-    0.0079461395724623237,
-    0.053280454055540001,
-    0.21797364640726541,
-    0.45496740582554868,
-    0.26521634184240478,
-]
-const fvals_exp = @[
-  0.002467115141477932,
-  0.018147435573256168,
-  0.07944063151366336,
-  0.27137669897479122,
-  0.79782256866993773,
-  2.1623306025075739,
-]
+const
+    pvals_exp = @[
+        0.00061601229677880041,
+        0.0079461395724623237,
+        0.053280454055540001,
+        0.21797364640726541,
+        0.45496740582554868,
+        0.26521634184240478,
+    ]
 
-const pvals_dev = @[
-  6.5288960012625658e-05,
-  0.00044199216814302695,
-  0.0020859587871659754,
-  0.0075913681418996841,
-  0.02260266219257237,
-  0.056532254390212859,
-  0.11939049233042602,
-  0.20969545753234975,
-  0.29254151133139222,
-  0.28905301416582552,
-]
+    fvals_exp = @[
+      0.002467115141477932,
+      0.018147435573256168,
+      0.07944063151366336,
+      0.27137669897479122,
+      0.79782256866993773,
+      2.1623306025075739,
+    ]
 
-const fvals_dev = @[
-  3.068330909892871e-07,
-  3.551788624668698e-06,
-  2.542810833482682e-05,
-  0.0001466508940804874,
-  0.0007457199853069548,
-  0.003544702600428794,
-  0.01648881157673708,
-  0.07893194619504579,
-  0.4203787615506401,
-  3.055782252301236,
-]
+    pvals_dev = @[
+      6.5288960012625658e-05,
+      0.00044199216814302695,
+      0.0020859587871659754,
+      0.0075913681418996841,
+      0.02260266219257237,
+      0.056532254390212859,
+      0.11939049233042602,
+      0.20969545753234975,
+      0.29254151133139222,
+      0.28905301416582552,
+    ]
 
-const pvals_turb = @[
-  0.596510042804182,
-  0.4034898268889178,
-  1.303069003078001e-07,
-]
+    fvals_dev = @[
+      3.068330909892871e-07,
+      3.551788624668698e-06,
+      2.542810833482682e-05,
+      0.0001466508940804874,
+      0.0007457199853069548,
+      0.003544702600428794,
+      0.01648881157673708,
+      0.07893194619504579,
+      0.4203787615506401,
+      3.055782252301236,
+    ]
 
-const fvals_turb = @[
-  0.5793612389470884,
-  1.621860687127999,
-  7.019347162356363,
-]
+    pvals_turb = @[
+      0.596510042804182,
+      0.4034898268889178,
+      1.303069003078001e-07,
+    ]
 
-const pvals_gauss = @[1.0]
-const fvals_gauss = @[1.0]
+    fvals_turb = @[
+      0.5793612389470884,
+      1.621860687127999,
+      7.019347162356363,
+    ]
+
+    pvals_gauss = @[1.0]
+    fvals_gauss = @[1.0]
 
 
 
@@ -194,6 +197,12 @@ proc newGMixSimple(pars: GMixSimplePars) : GMix =
 #proc newGMixCoellip(pars: GMixPars, num: int) : GMix =
 #    newSeq(result, num) 
 
+iterator mitems(self: var GMix): var Gauss2D =
+    ##
+    var i = 0
+    while i < self.len:
+        yield self[i]
+        inc i
 
 when isMainModule:
 
@@ -202,7 +211,14 @@ when isMainModule:
     # no copy made here (see nimcache)
     for i in 0..gm.len-1:
         let fi=float(i)
-        gm[i].init(1.0+fi, 15.0+fi, 16.0+fi, 4.5, 0.2, 6.7)
+        gm[i].init(
+            p=1.0+fi,
+            row=15.0+fi,
+            col=16.0+fi,
+            irr=4.5,
+            irc=0.2,
+            icc=6.7,
+        )
 
     echo("gm:\n",gm)
 
@@ -211,6 +227,27 @@ when isMainModule:
     echo("gm again:")
     for g in gm:
         echo(g)
+
+    # mitems iterates, returning modifiable items; under the hood g is a
+    # pointer to the Gauss2D
+    echo("modifying in loop:")
+    for g in mitems(gm):
+
+        # This time show hout to use a Gauss2DPars note only a single stack
+        # allocated structure is reused each time
+
+        let pars: Gauss2DPars = (
+            p:   random(1.0..2.0),
+            row: 15.0 + random(-1.0..1.0),
+            col: 16.0 + random(-1.0..1.0),
+            irr: 2.0*(1.0 + 0.1*random(-1.0..1.0)),
+            irc: random(-0.1..0.1),
+            icc: 3.0*(1.0 + 0.1*random(-1.0..1.0)),
+        )
+
+        g.init(pars)
+
+    echo("gm:\n",gm)
 
     echo("ngauss for exp is: ",nGaussTable[GMixModel.exp])
 
